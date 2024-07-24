7b6caaad27dcfcd220584b02dfbc970661c87a72
rajkumar@123 <160397769+rajkumar-chauhan@users.noreply.github.com>
2024-07-24 14:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,155 @@
-#Redis
+resource "aws_vpc" "ot_microservices_dev" {
+  cidr_block       = "10.0.0.0/25"
+  instance_tenancy = "default"
+  tags = {
+    Name = "ot-micro-vpc"
+  }
+}
 
-## Redis Security group
+resource "aws_security_group" "bastion_security_group" {
+  vpc_id = aws_vpc.ot_microservices_dev.id
+  name   = "bastion-security-group"
+
+  tags = {
+    Name = "bastion-security-group"
+  }
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_security_group" {
+  vpc_id = aws_vpc.ot_microservices_dev.id
+  name   = "alb-security-group"
+
+  tags = {
+    Name = "alb-security-group"
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "employee_security_group" {
+  vpc_id      = aws_vpc.ot_microservices_dev.id
+  name        = "employee-security-group"
+  description = "Security group for employee instance"
+  
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "employee-security-group"
+  }
+}
+
+resource "aws_security_group" "salary_security_group" {
+  vpc_id      = aws_vpc.ot_microservices_dev.id
+  name        = "salary-security-group"
+  description = "Security group for salary instance"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_security_group.id]
+  }
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "salary-security-group"
+  }
+}
+
+resource "aws_security_group" "attendance_security_group" {
+  vpc_id = aws_vpc.ot_microservices_dev.id
+  name   = "attendance-security-group"
+
+  tags = {
+    Name = "attendance-security-group"
+  }
+  
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_security_group" "redis_security_group" {
   vpc_id      = aws_vpc.ot_microservices_dev.id
@@ -22,7 +171,7 @@ resource "aws_security_group" "redis_security_group" {
     protocol       = "tcp"
     security_groups = [aws_security_group.attendance_security_group.id]
   }
-
+  
   ingress {
     description    = "Allow Redis"
     from_port      = 6379
@@ -30,7 +179,7 @@ resource "aws_security_group" "redis_security_group" {
     protocol       = "tcp"
     security_groups = [aws_security_group.salary_security_group.id]
   }
-
+  
   ingress {
     description    = "Allow Redis"
     from_port      = 6379
@@ -39,7 +188,7 @@ resource "aws_security_group" "redis_security_group" {
     security_groups = [aws_security_group.employee_security_group.id]
   }
 
-    egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -51,10 +200,16 @@ resource "aws_security_group" "redis_security_group" {
   }
 }
 
-## Redis instance 
+resource "aws_subnet" "database_subnet" {
+ vpc_id            = aws_vpc.ot_microservices_dev.id
+ cidr_block        = "10.0.0.64/28"
+ availability_zone = "us-east-2a"
+ tags = {
+   Name = "Database Subnet"
+ }
+}
 
 resource "aws_instance" "Redis_instance" {
-  # ami to be replaced with actual ami
   ami           = "ami-0862be96e41dcbf74"
   subnet_id = aws_subnet.database_subnet.id
   key_name = "backendp9"
@@ -65,8 +220,3 @@ resource "aws_instance" "Redis_instance" {
     Name = "Redis"
   }
 }
-
-
-
-                                                                                                                                                   
-
